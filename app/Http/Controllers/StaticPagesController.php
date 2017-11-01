26e8fccbfa251b0e54f1f197393b7572263dcad6
wenjian <wenjian337@gmail.com>
2017-11-01 08:42:22 +0000

    add static page

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -7,17 +7,17 @@ use Illuminate\Http\Request;
 class StaticPagesController extends Controller
 {
     public function home(){
-      return view('static_pages/home');
+      return view('static_page/home');
       return "主页";
     }
 
     public function help(){
-      return view('static_pages/help');
+      return view('static_page/help');
       return "帮助页";
     }
 
     public function about(){
-      return view('static_pages/about');
+      return view('static_page/about');
       return "关于我们";
     }
 }
